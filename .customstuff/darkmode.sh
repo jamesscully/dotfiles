1cebe68666593a558b696c94c00dd2898286981f
psyjs20 <psyjs20@exmail.nottingham.ac.uk>
2019-05-17 16:34:05 +0100

    Updated configs; darkmode now does not require a file for text, --no-toggle added

--- .customstuff/darkmode.sh
+++ .customstuff/darkmode.sh
@@ -13,28 +13,53 @@ darktext=$install_dir/dark_text
 walldark=$install_dir/wallpaper_dark.jpg
 walllight=$install_dir/wallpaper.jpg
 
+wallpaper=$walldark
+
 status=$(cat "$darkswitch")
 
-#if [ "$1" = "--no-toggle" ]; then
-#	if [ "$status" = "1" ]; then feh --bg-fill "$walllight"
-#	elif [ "$status" = "0"]; then feh --bg-fill "$walldark"
-#	
-#	
-#fi
 
-if [ "$status" = "1" ]; then
-	feh --bg-fill "$walllight" &
-	echo "Light" > $darktext
-elif [ "$status" = "0" ]; then
-	feh --bg-fill "$walldark" &
-	echo "Dark" > $darktext
-else 
-	echo "Error in $darkswitch; only 0 or 1 permitted"
+
+## echoes the status i.e. Dark / Light to stdout
+printStatus() {
+	if [ "$status" = "0" ]; then 
+		echo "Dark"
+	else 
+		echo "Light"
+	fi
 	exit
+}
+
+## actually sets the background
+setBackground() {
+	feh --bg-fill "$wallpaper" &
+}
+
+## load $wallpaper variable
+getBackground() {
+	if [ "$status" = "1" ]; then
+	       	wallpaper=$walldark
+        elif [ "$status" = "0" ]; then
+        	wallpaper=$walllight
+        else 
+        	echo "Error in $darkswitch; only 0 or 1 permitted"
+        	exit
+        fi
+}
+
+if [ "$1" = "--status" ]; then
+	printStatus
 fi
 
+getBackground
+setBackground
+
+
+## don't flip if we're not toggling
+if [ "$1" = "--no-toggle" ]; then exit; fi
 
 # flip dark status
 ((status ^= 1))
 
 echo $status > $darkswitch
+echo $status
+
